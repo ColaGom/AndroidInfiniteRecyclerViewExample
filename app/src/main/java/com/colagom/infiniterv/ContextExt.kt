d513b7fc94cb6b924c1b6d9513fb38a9818117dc
ColaGom <geunho@flitto.com>
2019-09-03 19:03:05 +0900

    feat: Implement Context Extension

--- app/src/main/java/com/colagom/infiniterv/ContextExt.kt
+++ app/src/main/java/com/colagom/infiniterv/ContextExt.kt
@@ -1,2 +1,13 @@
 package com.colagom.infiniterv
 
+import android.content.Context
+import android.util.DisplayMetrics
+
+fun Context.dpToPx(dp: Float): Float {
+    return dp * getPixelScaleFactor(this)
+}
+
+private fun getPixelScaleFactor(context: Context): Float {
+    val displayMetrics = context.resources.displayMetrics
+    return displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT
+}
